041f82d7a8e91205ce0ef97446cb0db779f809c6
byte-glass <m.campbell@tutanota.com>
2024-03-25 15:02:49 +0000

    add a sink for testing

--- carolina/src/a.ml
+++ carolina/src/a.ml
@@ -13,13 +13,24 @@ rlwrap -a ocaml -noinit
 
 *)
 
+#use "topfind"
+#require "domainslib"
+
 open Domainslib
 
 module C = Domainslib.Chan
 
-type 'a message = Candidate of 'a | Finished of int
+type 'a message =  Threshold of 'a | Candidate of 'a | Finished of int
+(* type 'b threshold = Threshold of 'b  -- can't get this to work!! *)
 
 type 'a channels = {snd : 'a C.t; rcv : 'a C.t}
+
+(* assume u < x.(Array.length x - 1) *)
+let insert x u =
+    let i = ref 0 in
+    while u > x.(!i) do i := !i + 1 done;
+    for j = Array.length x - 1 downto !i + 1 do x.(j) <- x.(j - 1) done;
+    x.(!i) <- u
 
 let tally_fun ch u =
     fun () ->
@@ -27,9 +38,15 @@ let tally_fun ch u =
             match C.recv ch.rcv with
             | Candidate t ->
                     Printf.printf "tally: candidate %f\n" t;
+                    if t < u.(Array.length u - 1) then
+                        let _ = insert u t in
+                        C.send ch.snd (Threshold u.(Array.length u - 1));
                     tally u
             | Finished id ->
                     Printf.printf "tally: finished %d\n" id;
+                    () 
+            | Threshold t -> 
+                    Printf.printf "tally: threshold!??\n"; 
                     () in
         tally u
 
@@ -45,21 +62,33 @@ let n = 10
 let u = Array.init n (fun _ -> Random.float 1.0)
 let () = Array.sort Float.compare u
 
-let tally_d = Domain.spawn(tally_fun ch u)
+let tally_d = Domain.spawn(tally_fun {snd = a; rcv = b} u)
 
-let tally_candidate = Domain.spawn(fun _ -> C.send ch.rcv (Candidate 0.1))
-let tally_finished = Domain.spawn(fun _ -> C.send ch.rcv (Finished 11))
+ Domain.spawn(fun _ -> C.send b (Finished 11))
+
+Domain.spawn(fun _ -> C.send b (Candidate 0.2))
+let sink_d = Domain.spawn(sink_fun a)
+
 
 Domain.join tally_d
 
 *)
 
 
-
-
-
-let () = print_endline "Oh, Carolina!"
-
+let sink_fun c =
+    fun () ->
+        let rec sink c = 
+        match C.recv c with
+            | Candidate t ->
+                    Printf.printf "sink: candidate %f\n" t;
+                    sink c
+            | Threshold t -> 
+                    Printf.printf "sink: threshold %f\n" t;
+                    sink c
+            | Finished id ->
+                    Printf.printf "sink: finished %d\n" id;
+                    () in
+        sink c
 
 (* end *)
 
