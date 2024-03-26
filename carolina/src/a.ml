184afd87ecba3703cefff18c77d6eba97979df78
byte-glass <m.campbell@tutanota.com>
2024-03-26 18:14:46 +0000

    two workers - behaviour not as expected

--- carolina/src/a.ml
+++ carolina/src/a.ml
@@ -23,7 +23,10 @@ module C = Domainslib.Chan
 type 'a worker_message = Candidate of 'a | Finished of int
 type 'b tally_message = Threshold of 'b 
 
-type ('a, 'b) channels = {snd : 'a C.t; rcv : 'b C.t}
+(* type ('a, 'b) channels = {snd : 'a C.t; rcv : 'b C.t} *)
+type ('a, 'b) channels = {snd : 'a; rcv : 'b}
+
+type interval = {lower: int; upper: int} 
 
 (* assume u < x.(Array.length x - 1) *)
 let insert x u =
@@ -34,45 +37,74 @@ let insert x u =
 
 let tally_fun ch u =
     fun () ->
-        let rec tally u =
-            match C.recv ch.rcv with
-            | Candidate t ->
-                    Printf.printf "tally: candidate %f\n" t;
-                    if t < u.(Array.length u - 1) then
-                        let _ = insert u t in
-                        C.send ch.snd (Threshold u.(Array.length u - 1));
-                    tally u
-            | Finished id ->
-                    Printf.printf "tally: finished %d\n" id;
-                    ()  in
-        tally u
+        let rec tally f u =
+            if List.length f = List.length ch.snd then
+                ()
+            else
+                match C.recv ch.rcv with
+                | Candidate t ->
+                        Printf.printf "tally: candidate %f\n%!" t;
+                        if t < u.(Array.length u - 1) then
+                            let _ = insert u t in
+                            let th = Threshold u.(Array.length u - 1) in
+                            List.iter (fun c -> C.send c th) ch.snd;
+                        tally f u
+                | Finished id ->
+                        Printf.printf "tally: finished %d\n%!" id;
+                        tally ((Finished id) :: f) u in
+        tally [] u
 
 
-let update c theta =
+let update c theta id =
     let rec upd th =
         match C.recv_poll c with 
         | Some (Threshold t) -> th := t; upd th
         | None -> () in
     let th = ref theta in
     upd th; 
-    Printf.printf "update: %f -> %f\n" theta !th;
+    Printf.printf "update: %d: %f -> %f\n%!" id theta !th;
     !th
 
 
-let worker_fun ch x i theta =
+let worker_fun ch x bounds i theta =
     fun () ->
         let rec worker i theta = 
-            if i > Array.length x - 1 then
+            if i >= bounds.upper then
                 let _ = C.send ch.snd (Finished (Domain.self () :> int)) in ()
             else
                 let j = ref i in
-                let _ = while (!j < Array.length x - 1) && (x.(!j) >= theta) do j := !j + 1 done in
+                let _ = while (!j < bounds.upper - 1) && (x.(!j) >= theta) do 
+                    Printf.printf "%d %f\n%!" (Domain.self () :> int) x.(!j);
+                    j := !j + 1 
+                done in
                 if x.(!j) < theta then
                     let _ = C.send ch.snd (Candidate x.(!j)) in
-                    worker (!j + 1) (update ch.rcv theta)
+                    worker (!j + 1) (update ch.rcv theta (Domain.self() :> int))
                 else 
                     worker (!j + 1) theta in
-        worker i theta
+        worker bounds.lower theta
+
+(* two workers!
+
+Random.init 103
+let n = 10
+let x = Array.init (3 * n) (fun _ -> Random.float 1.0)
+
+let u = Array.init n (fun i -> x.(i))
+let () = Array.sort Float.compare u
+let theta = u.(Array.length u - 1)
+
+let a0 = C.make_unbounded () and
+    a1 = C.make_unbounded () and
+    b = C.make_unbounded () in
+    let domains = [(tally_fun {snd = [a0; a1]; rcv = b} u); 
+            (worker_fun {snd = b; rcv = a0} x {lower = n; upper = 2 * n} 0 theta);
+            (worker_fun {snd = b; rcv = a1} x {lower = 2 * n; upper = 3 * n} n theta)] in
+    List.map (fun d -> Domain.spawn(d)) domains
+
+
+
+*)
 
 (* test worker_fun and tally_fun
 
@@ -90,7 +122,7 @@ let n = 15
 let x = Array.init n (fun _ -> Random.float 1.0)
 
 let tally_d = Domain.spawn(tally_fun {snd = a; rcv = b} u)
-let worker_d = Domain.spawn(worker_fun {snd = b; rcv = a} x 0 theta)
+let worker_d = Domain.spawn(worker_fun {snd = b; rcv = a} x {lower = 0; upper = Array.length x} 0 theta)
 
 *)
 
@@ -139,7 +171,7 @@ let sink_tally_fun c =
         let rec sink c = 
             match C.recv c with
             | Threshold t -> 
-                    Printf.printf "sink: threshold %f\n" t;
+                    Printf.printf "sink: threshold %f\n%!" t;
                     sink c in
         sink c
 
@@ -148,10 +180,10 @@ let sink_worker_fun c =
         let rec sink c = 
         match C.recv c with
             | Candidate t ->
-                    Printf.printf "sink: candidate %f\n" t;
+                    Printf.printf "sink: candidate %f\n%!" t;
                     sink c
             | Finished id ->
-                    Printf.printf "sink: finished %d\n" id;
+                    Printf.printf "sink: finished %d\n%!" id;
                     () in
         sink c
 
