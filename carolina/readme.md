21db4e84ba1af4a370018add7b76f6cdbbe648e5
byte-glass <m.campbell@tutanota.com>
2024-04-05 14:01:56 +0100

    working version, ugly imperative style

--- carolina/readme.md
+++ carolina/readme.md
@@ -2,15 +2,16 @@
 
 ## latest
 
- - src/a.ml - tally spawns workers, two way communication seems to work.
+ - src/a.ml -- early working version in imperative style
+ - write function to read all available `Threshold` messages from the channel
+ - have workers run over slices of an array
+ - have fun tally spawn workers, try more imperative style if it makes workers fall out more easily
 
  - tidy up src/v0
  - logging and more than one worker - behaviour still baffling
 
 ## next steps
 
- - have workers run over slices of an array
- - have fun tally spawn workers, try more imperative style if it makes workers fall out more easily
 
  - how to set up tests
  - get better acquainted with dune see https://dune.readthedocs.io/en/stable/quick-start.html
--- carolina/src/a.ml
+++ carolina/src/a.ml
@@ -26,30 +26,73 @@ type 'b tally_message = Threshold of 'b
 
 type ('a, 'b) channels = {snd : 'a C.t; rcv : 'b C.t}
 
-let worker_fun ch =
+type 'a interval = {upper: 'a; lower: 'a}
+
+let get_threshold c =
+    let t = match C.recv c with | Threshold t -> t in
+    let th = ref t in
+    let rec upd () =
+        match C.recv_poll c with 
+        | Some (Threshold t) -> th := t; upd ()
+        | None -> () in
+    upd ();
+    !th
+
+let worker_fun ch x bounds =
     fun () ->
         begin
             let id = string_of_int (Domain.self () :> int) in 
             logger#debug "worker: %s" id;
-            match C.recv ch.rcv with 
-            | Threshold t -> logger#debug "worker_fun: %s received threshold %f" id t;
-            C.send ch.snd (Candidate (-1.0));
+            let th = ref x.(bounds.lower) in
+            th := get_threshold ch.rcv;
+            logger#debug "worker: %s threshold is %f" id !th;
+            let i = ref bounds.lower in
+            while !i < bounds.upper do
+                if x.(!i) < !th then begin
+                    C.send ch.snd (Candidate x.(!i));
+                    logger#debug "worker_fun: %s send candiate %f" id x.(!i);
+                    th := get_threshold ch.rcv;
+                    logger#debug "worker_fun: %s threshold is %f" id !th;
+                end;
+                i := !i + 1
+            done;
             C.send ch.snd (Finished id);
-            ()
+            logger#debug "worker_fun: %s send finished" id
         end
 
+let last u = u.(Array.length u - 1)
 
-let tally () =
+(* assume v < last u *)
+let insert u v =
+    let i = ref 0 in
+    while v >= u.(!i) do i := !i + 1 done;
+    for j = Array.length u - 1 downto !i + 1 do u.(j) <- u.(j - 1) done;
+    u.(!i) <- v
+
+let tally u x n =
     begin
-        let n = 1 in
         let a = C.make_unbounded () and
             bs = Array.init n (fun _ -> C.make_unbounded ()) in
-        let workers = Array.map (fun b -> Domain.spawn(worker_fun {snd = a; rcv = b;})) bs in
-        Array.iter (fun b -> C.send b (Threshold 1.0)) bs;
+        let k = (Array.length x) / n in
+        let _ = logger#debug "tally: k is %d" k in
+        let workers = 
+            Array.init n (fun i -> Domain.spawn(worker_fun {snd = a; rcv = bs.(i)} x {lower = i * k; upper = (i + 1) * k})) in
+        Array.iter (fun b -> C.send b (Threshold (last u))) bs;
+        logger#debug "tally: send threshold %f" (last u);
         let finished = ref [] in
         while List.length !finished != n do
             match C.recv a with 
-            | Candidate t -> logger#debug "tally: received candidate %f" t;
+            | Candidate t -> begin
+                logger#debug "tally: received candidate %f" t;
+                if t < last u then begin
+                    insert u t;
+                    logger#debug "tally: insert %f" t;
+                    Array.iter (fun b -> C.send b (Threshold (last u))) bs;
+                    logger#debug "tally: send threshold %f" (last u)
+                end
+                else 
+                    logger#debug "tally: too late!! %f is above the threshold" t
+            end
             | Finished id -> 
                     logger#debug "tally: received finished %s" id;
                     finished := id :: !finished
@@ -59,7 +102,19 @@ let tally () =
 
 let main () = 
     begin
-        tally ();
+        let () = Random.init 103 and
+            k = 100 and
+            n = 10 in
+        let z = Array.init ((n + 1) * k) (fun _ -> Random.float 1.0) in
+        let u = Array.init k (fun i -> z.(i)) in
+        let _ = Array.sort Float.compare u and
+            x = Array.init (n * k) (fun i -> z.(i + k)) in
+        (* Array.iter (fun v -> logger#debug "u: %f" v) u;
+        Array.iter (fun v -> logger#debug "x: %f" v) x;  *)
+        tally u x n;
+        Array.iter (fun v -> logger#debug "u: %f" v) u;
+        Array.sort Float.compare z;
+        Array.iter (fun v -> logger#debug "z: %f" v) z;
         logger#debug "ok\n";
         Printf.printf "ok\n"
     end
